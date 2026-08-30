c220790957096504e3bd97bfe647c24ab1f7951e
Ennie-CL <cleo446688@gmail.com>
2026-08-30 13:37:37 +0100

    adding file

---
+++ calculator.py
@@ -0,0 +1,26 @@
+import numpy
+
+
+class calculator:
+
+    def __init__(self):
+        pass
+
+
+
+    def adding(self):
+        pass
+
+
+    def subbing(self):
+        pass
+
+    def multi(self):
+        pass
+
+    def dividing(self):
+        pass
+
+    def powering(self):
+        pass
+
