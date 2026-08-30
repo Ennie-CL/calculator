4dc787d48392e814f0904d5e60c9eca3917d1f51
Ennie-CL <cleo446688@gmail.com>
2026-08-30 14:43:57 +0100

    basic calculator
    completed functions of a basic calculator
    between 2 numbers

--- calculator.py
+++ calculator.py
@@ -4,33 +4,51 @@ import numpy
 class calculator:
 
     def __init__(self,n1 ,oper ,n2):
-        self.n1 = n1
+        self.n1 = int(n1)
         self.operation = oper
-        self.n2 = n2
+        self.n2 = int(n2)
 
 
 
     def adding(self):
-        pass
 
-
-    def subbing(self):
-        pass
+        result = self.n1 + self.n2
+        return result
     
 
+    def subbing(self):
+        result = self.n1 - self.n2
+        return result
+
     def multi(self):
-        pass
+        result = self.n1 * self.n2
+        return result
     
 
     def dividing(self):
-        pass
+        if self.n2 == 0:
+            print("cannot divide by zero")
+            return
+        result = self.n1 / self.n2
+        return result
+
+    def calculation(self):
+
+        if self.operation == '+':
+            result = self.adding()
+        elif self.operation == "-":
+            result = self.subbing()
+        elif self.operation == "*":
+            result = self.multi()
+        elif self.operation == "/":
+            result = self.dividing()
+
+        print(result)
 
 
-
-
-num1 = print("Enter first number: ")
-operator = print("Choose an operation (+, -, *, /): ")
-num2 = print("Enter second number: ")
+num1 = input("Enter first number: ")
+operator = input("Choose an operation (+, -, *, /): ")
+num2 = input("Enter second number: ")
 
 test = calculator(num1, operator, num2)
-
+test.calculation()
