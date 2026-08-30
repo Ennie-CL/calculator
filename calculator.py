5dd629744790496eaf6f0494b3032449ae64a462
Ennie-CL <cleo446688@gmail.com>
2026-08-30 15:36:16 +0100

    error handling
    added error handling for the inputs

--- calculator.py
+++ calculator.py
@@ -4,10 +4,10 @@ import numpy
 class calculator:
 
     def __init__(self,n1 ,oper ,n2):
+
         self.n1 = int(n1)
         self.operation = oper
         self.n2 = int(n2)
-
 
 
     def adding(self):
@@ -36,19 +36,50 @@ class calculator:
 
         if self.operation == '+':
             result = self.adding()
-        elif self.operation == "-":
+        elif self.operation == '-':
             result = self.subbing()
-        elif self.operation == "*":
+        elif self.operation == '*':
             result = self.multi()
-        elif self.operation == "/":
+        elif self.operation == '/':
             result = self.dividing()
 
         print(result)
 
 
-num1 = input("Enter first number: ")
-operator = input("Choose an operation (+, -, *, /): ")
-num2 = input("Enter second number: ")
+num1 = 0
+while True:
+  try:
+     num1 = int(input("Enter first number: "))
+  except ValueError:
+     print("Not an integer!")
+     continue
+  else:
+     break
+
+operator = ''
+while True:
+    operator = input("Choose an operation (+, -, *, /): ")
+    if operator == "+":
+        break
+    if operator == "-":
+        break
+    if operator == "*":
+        break
+    if operator == "/":
+        break
+    else:
+        print("Not an operation!")
+
+num2 = 0
+while True:
+  try:
+     num2 = int(input("Enter second number: "))
+  except ValueError:
+     print("Not an integer!")
+     continue
+  else:
+     break
+
 
 test = calculator(num1, operator, num2)
 test.calculation()
